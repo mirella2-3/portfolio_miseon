ba084ad635af83db7ad2ad4f27ac4894268c540b
mirella2-3 <zmeo89@gmail.com>
2025-10-29 02:29:06 +0900

    언제수정돼

--- src/assets/SkillList.js
+++ src/assets/SkillList.js
@@ -1,9 +1,3 @@
-const SkillList = [
-    '/images/Skill-image/Skill-image01.png',
-    '/images/Skill-image/Skill-image01.png',
-    '/images/Skill-image/Skill-image01.png',
-    '/images/Skill-image/Skill-image01.png',
-    '/images/Skill-image/Skill-image01.png',
-];
+const SkillList = ['/images/Skill-image/Skill-image01.png'];
 
 export default SkillList;
