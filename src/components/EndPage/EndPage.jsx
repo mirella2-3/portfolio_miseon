fe16ee4637ea8272b42e8aea9cdb5d00963bd96f
mirella2-3 <zmeo89@gmail.com>
2025-11-01 02:59:25 +0900

    마지막사진엑박

--- src/components/EndPage/EndPage.jsx
+++ src/components/EndPage/EndPage.jsx
@@ -9,7 +9,7 @@ const EndPage = () => {
                 </div>
                 <div className="text-box">
                     <p>
-                        <img src="/images/endText.png" alt="" />
+                        <img src="/images/Aboutme-image/coffee.png" alt="" />
                     </p>
 
                     <span>내 작품이 커피 향처럼 오래 머물기를 바랍니다.</span>
