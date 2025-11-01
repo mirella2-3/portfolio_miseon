42bc58794e583d6454251795100eeab2f749a948
mirella2-3 <zmeo89@gmail.com>
2025-11-01 21:47:16 +0900

    인트로수정2

--- src/common/footer/index.jsx
+++ src/common/footer/index.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <FooterStyle>
             <div className="inner">
                 <ul>
-                    <li>강미선</li>
+                    <li>강미선 </li>
                     <li>Email : m1384@naver.com</li>
                     <li>Phone : 010-2278-1384</li>
                 </ul>
