f2f56952ff95c2159559d64b2aea790125acb332
mirella2-3 <zmeo89@gmail.com>
2025-11-01 23:14:28 +0900

    ㅈㅂ1

---
+++ src/common/Footer/index.jsx
@@ -0,0 +1,16 @@
+import { FooterStyle } from './style';
+
+const Footer = () => {
+    return (
+        <FooterStyle>
+            <div className="inner">
+                <ul>
+                    <li>강미선 </li>
+                    <li>Email : m1384@naver.com</li>
+                    <li>Phone : 010-2278-1384</li>
+                </ul>
+            </div>
+        </FooterStyle>
+    );
+};
+export default Footer;
